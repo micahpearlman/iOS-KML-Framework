b70f08bc782452df7d260b72cf2830c3cacbfb1e
Micah Pearlman <micahpearlman@gmail.com>
2014-01-04 10:12:47 -0800

    build library instead of framework

---
+++ kml-iosTests/kml_iosTests.m
@@ -0,0 +1,34 @@
+//
+//  kml_iosTests.m
+//  kml-iosTests
+//
+//  Created by Micah Pearlman on 1/3/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface kml_iosTests : XCTestCase
+
+@end
+
+@implementation kml_iosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
